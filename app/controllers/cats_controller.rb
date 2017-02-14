699df417b12d2de20ace175d8f435a5239c76e86
App Academy Student <student@appacademy.io>
2017-02-14 12:16:17 -0800

    add edit form

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -28,6 +28,22 @@ class CatsController < ApplicationController
     end
   end
 
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
+
 
 
   private
